383b6bd313d1a6551c824b813b996bd448b3a233
sarbajit <sarbajit>
2025-08-04 21:23:10 +0530

    Extra changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Player from './pages/Player'; // You'll create this tomorrow
-import Navbar from "./components/Navbar"
-function App() {
+
+import Home from "./pages/Home";
+import Musicplayer from "./pages/Player";
+// import About from "./pages/About";
+import MainLayout from "./pages/MainLayout";
+
+const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Landing/Home page - No Navbar */}
         <Route path="/" element={<Home />} />
-        <Route path="/Navbar" element={<Navbar />} />
 
-        {/* <Route path="/player" element={<Player />} /> */}
+        {/* All pages after continue - with Navbar using layout */}
+        <Route element={<MainLayout />}>
+          <Route path="/musicplayer" element={<Musicplayer />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* Add more routes here */}
+        </Route>
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
